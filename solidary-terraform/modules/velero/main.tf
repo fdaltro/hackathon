f4074fb47358aacfd8284e1801e4658e15049b27
Fabricio Daltro <fabriciodaltro@gmail.com>
2026-07-14 20:52:45 -0300

    Correção td velero

--- solidary-terraform/modules/velero/main.tf
+++ solidary-terraform/modules/velero/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
+
 # ==========================================================
 # Estratégia de DR (Opção A - Multicloud/Cross-Region Backup):
 # Velero fazendo backup do estado do cluster (manifestos +
